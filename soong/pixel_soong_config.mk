b9e635c557a32fdcb076e5ade865573c10f5b3a9
Sasha Smundak <asmundak@google.com>
2021-08-30 18:19:28 -0700

    Instead of using add_soong_config_xxx macros, just specify the namespace.
    Fixes: 197778346
    Test: treehugger
    Change-Id: Ib03a197fcfbc4993a44357e1d6862da67ad14323

--- soong/pixel_soong_config.mk
+++ soong/pixel_soong_config.mk
@@ -2,6 +2,10 @@
 # Set the variables properly in ProductConfig Makefiles for each pixel device
 
 #For sensor
-$(call add_soong_config_namespace,sensor)
-$(call add_soong_config_var_value,sensor,mag_no_temp,true)
-$(call add_soong_config_var_value,sensor,single_light,true)
+SOONG_CONFIG_NAMESPACES += sensor
+SOONG_CONFIG_sensor += \
+        mag_no_temp \
+        single_light
+
+SOONG_CONFIG_sensor_mag_no_temp := true
+SOONG_CONFIG_sensor_single_light := true

3339fdc32311bb60a4109b4f9251bce4813b1864
flawedworld <flawedworld@flawed.world>
2021-09-29 04:51:41 +0100

    Use APNs and CarrierConfig from APV

--- barbet.mk
+++ barbet.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/barbet/aosp_barbet.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := barbet
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true

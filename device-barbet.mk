f44259dfe26ca6c58130899e7a8eb270dd6608ce
Badhri Jagan Sridharan <badhri@google.com>
2022-01-31 22:15:15 -0800

    Migrate IUsb implementation to AIDL
    This change migrates IUsb implementation to AIDL.
    Also, IUsb and IUsbGadget now run in its own processes
    to improve stability and isolation.
    
    Port of 3c97a6bb68f6418ee41026f3bd0024405c97cf0c
    
    Bug: 200993386
    Change-Id: I681c1ba9c4f547e3cf5dc0fa7c9ee1aaf4fd603c
    Merged-In: I681c1ba9c4f547e3cf5dc0fa7c9ee1aaf4fd603c
    (cherry picked from commit 6708ba42477ff7f7d3afcc8f6a160a1e67b9ed60)

--- device-barbet.mk
+++ device-barbet.mk
@@ -109,8 +109,11 @@ ifeq ($(wildcard vendor/google_devices/barbet/proprietary/device-vendor-barbet.m
     BUILD_WITHOUT_VENDOR := true
 endif
 
+# USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.barbet
+    android.hardware.usb-service.barbet
+PRODUCT_PACKAGES += \
+    android.hardware.usb.gadget-service.barbet
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \

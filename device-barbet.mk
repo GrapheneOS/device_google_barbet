922afa98ba673cfbe4e7558d4ac63b577e051e40
flawedworld <flawedworld@flawed.world>
2022-04-13 20:01:31 +0100

    Add aggregated sysconfig xml with items from stock OS

--- device-barbet.mk
+++ device-barbet.mk
@@ -205,3 +205,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     device/google/barbet/default-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions.xml
+
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
